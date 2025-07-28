06f05b3ca26e3b177a8575a3c99b37f0b06c3ed6
ImendaM <realimendam@gmail.com>
2025-07-28 17:04:38 +0200

    Updated ProjectContainer.jsx

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -23,7 +23,7 @@ function Projects(){
                 <p className='font-[verdana]'>This is a description of my personal project.</p>
                 <div className='flex justify-center'>
                 <img src="/Github.png" alt="" className='w-[60px] lg:w-[100px]' />
-                <img src="/public/Arrow.png" alt="" className='w-[40px] self-center cursor-pointer hover:w-[50px] lg:hover:w-[70px] lg:hover:h-[60px] lg:w-[50px] lg:h-[50px]' />
+                <img src="/Arrow.png" alt="" className='w-[40px] self-center cursor-pointer hover:w-[50px] lg:hover:w-[70px] lg:hover:h-[60px] lg:w-[50px] lg:h-[50px]' />
                 </div>
             </div>
         </section>
