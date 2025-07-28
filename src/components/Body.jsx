5d92e2a4a0d813db71f4eab22799d59803878c22
ImendaM <realimendam@gmail.com>
2025-07-28 13:55:47 +0200

    Updated Body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,7 @@ import Skills from './Skills'
 function Body(){
     return(
         <>
-        <section className='flex justify-between items-center mt-[50px] mr-[10px] ml-[10px] lg:m-[100px]'>
+        <section className='flex  justify-between items-center mt-[50px] mr-[10px] ml-[10px] lg:m-[100px]'>
         <Personal/>
         <MyImage/>
         <Skills/>
